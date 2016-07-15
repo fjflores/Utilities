5b6c6bad86d54ae0ee21d6cdaf63b73dbab0c423
Francisco J. Flores <fjflores76@hotmail.com>
2016-07-15 17:43:17 -0400

    Fixes error in figure handle name.

--- ffsavefig.m
+++ ffsavefig.m
@@ -13,19 +13,19 @@ function ffsavefig( hFig, folder )
 % Saves the figure to the specified folder.
 
 % Save figure.
-set( fig1, 'PaperPositionMode', 'auto' )
+set( hFig, 'PaperPositionMode', 'auto' )
 % If folder does not exist, create it.
 figName = 'Pfc_layers_states';
 fig2save = fullfile( figFold );
 folderFlag = exist( figFold, 'dir' );
 if folderFlag == 7
     fprintf( 'Printing figure %s\n', figName );
-    print( fig1, fig2save, '-dpng', '-r300' )
+    print( hFig, fig2save, '-dpng', '-r300' )
     
 else
     fprintf( 'Figure directory did not exist. Creating...' )
     fprintf( '%s', figFold )
     mkdir( figFold )
-    print( fig1, fig2save, '-dpng', '-r600' )
+    print( hFig, fig2save, '-dpng', '-r600' )
     
 end
