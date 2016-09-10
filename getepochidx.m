c8ee4f98dc3cdb604cf809f030dd9d32780ca91c
Francisco J. Flores <fjflores76@hotmail.com>
2016-09-10 15:17:18 -0400

    Major improvement to getepochidx.
    Previously the function had some problems with rounding the timestamps. Fixed
    that.

--- getepochidx.m
+++ getepochidx.m
@@ -1,6 +1,6 @@
 function idx = getepochidx( ts, epochStart, epochLength )
 
-%GETEPOCHIDX get the indexes for a given epoch.
+%GETEPOCHIDX get the indexes for a given epoch (with ms precision).
 % 
 % Usage:
 % idx = getepochidx( ts, epochStart, epochLength )
@@ -15,32 +15,51 @@ function idx = getepochidx( ts, epochStart, epochLength )
 % idx = logical vector of the same length as ts. It has ones in the
 % positions of the desired epoch to extract.
 
-idx = false( length( ts ), 1 );
+% convert time values to integers, rounded to milliseconds resolution.
+tInt = floor( ts * 1000 );
+tStart = floor( epochStart * 1000 );
+tEnd = floor( epochLength .* 1000 ) + tStart;
 
-% deal with rounding problems derived from non-integer sampling rates
-Fs = 1 / abs( mean( diff( ts ) ) );
-
-% round Fs to the second decimal.
-Fs = ( round( Fs * 100 ) ./ 100 );
-
-% and now get dt
-dt = 1 ./ Fs;
-offset = ts( 1 );
-firstTime = ( epochStart - offset + 1 ) / dt;
-
-if firstTime < 0.5
-    idxStart = ceil( firstTime );
+% Check that firs time point is less or equal than startTime
+if tStart < tInt( 1 );
+    error( 'Epoch start time is lesser than the first timestamp' )
     
-else
-    idxStart = round( firstTime );
-    
-end
-nPoints = round( epochLength / dt );
-    
-if ( idxStart + nPoints ) <= length( ts );
-    idx( idxStart : idxStart + nPoints - 1 ) = true;
-    
-else
+elseif tEnd > tInt( end )
     error( 'Epoch extends beyond length of time vector' )
     
+else
+    % if everything is correct, proceed
+    idx = false( length( ts ), 1 ); % allocate idx vector.
+    rightTail = find( tInt >= tStart );
+    leftTail = find( tInt <= tEnd );
+    idx( rightTail( 1 ) : leftTail( end ) ) = true;
+    
 end
+
+
+
+    
+
+
+% deal with rounding problems derived from non-integer sampling rates
+% Fs = 1 / abs( mean( diff( ts ) ) );
+
+% round Fs to the second decimal.
+% Fs = ( round( Fs * 100 ) ./ 100 );
+
+% and now get dt
+% dt = 1 ./ Fs;
+% dt = tInt( 2 ) - tInt( 1 );
+% offset = ts( 1 );
+% % firstTime = ( epochStart - offset + 1 ) / dt;
+% 
+% if firstTime < 0.5
+%     idxStart = ceil( firstTime );
+%     
+% else
+%     idxStart = round( firstTime );
+%     
+% end
+% nPoints = round( epochLength / dt );
+    
+
