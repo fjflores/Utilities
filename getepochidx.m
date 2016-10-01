40b693e67f77e14eca5fe8880caaa3e91bc3c02e
Francisco J. Flores <fjflores76@hotmail.com>
2016-10-01 12:17:34 -0400

    Fixes the return of unequal legnth indexes for same time length.

--- getepochidx.m
+++ getepochidx.m
@@ -15,10 +15,10 @@ function idx = getepochidx( ts, epochStart, epochLength )
 % idx = logical vector of the same length as ts. It has ones in the
 % positions of the desired epoch to extract.
 
-% convert time values to integers, rounded to milliseconds resolution.
-tInt = round( ts .* 1000 );
-tStart = round( epochStart * 1000 );
-tEnd = round( epochLength .* 1000 ) + tStart;
+% convert time values to integers, rounded to microseconds resolution.
+tInt = round( ts .* 1e6 );
+tStart = round( epochStart * 1e6 );
+tEnd = round( epochLength .* 1e6 ) + tStart;
 
 % Check that firs time point is less or equal than startTime
 if tStart < tInt( 1 );
@@ -31,7 +31,13 @@ else
     % if everything is correct, proceed
     idx = false( length( ts ), 1 ); % allocate idx vector.
     rightTail = find( tInt >= tStart );
-    leftTail = find( tInt <= tEnd );
+    leftTail = find( tInt < tEnd );
+    
+    % Make sure that output is always even in length.
+    if isodd( length( leftTail ) )
+        leftTail = find( tInt <= tEnd );
+        
+    end
     idx( rightTail( 1 ) : leftTail( end ) ) = true;
     
 end
