04220cd299b32a99bb5ca953ed89e7f89edd5012
Francisco J. Flores <fjflores76@hotmail.com>
2017-12-01 16:22:59 -0500

    Updates to use tic instead of cputime.

--- disprog.m
+++ disprog.m
@@ -1,43 +1,28 @@
-function disprog(i,N,steps);
+function disprog( i, n, steps )
 %DISPROG Display progression of a loop.
 %	DISPROG(i,N,steps) displays the progression of a loop.
 %
-%	I     : loop variable
-%	N     : final value of i
+%	i     : loop variable
+%	n     : final value of i
 %	STEPS : number of displayed steps.
 %
 %	Example:
 %	 N=16; for i=1:N, disprog(i,N,5); end;
 
-%	F. Auger, August, December 1995.
-%       from an idea of R. Settineri.
-%	Copyright (c) 1996 by CNRS (France).
-%
-%  This program is free software; you can redistribute it and/or modify
-%  it under the terms of the GNU General Public License as published by
-%  the Free Software Foundation; either version 2 of the License, or
-%  (at your option) any later version.
-%
-%  This program is distributed in the hope that it will be useful,
-%  but WITHOUT ANY WARRANTY; without even the implied warranty of
-%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%  GNU General Public License for more details.
-%
-%  You should have received a copy of the GNU General Public License
-%  along with this program; if not, write to the Free Software
-%  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+global start
 
-global begin_time_disprog ;
+if i == 1
+    start = tic;
+    
+end
 
-if (i==1),
- begin_time_disprog=cputime;
-end;
-
-if (i==N),
- fprintf('100 %% complete in %g seconds.\n', cputime-begin_time_disprog);
- clear begin_time_disprog;
-elseif (floor(i*steps/N)~=floor((i-1)*steps/N)),
- fprintf(' %g ', floor(i*steps/N)*ceil(100/steps));
-end;
+if i == n
+    fprintf( ' 100%% complete in %.2f seconds.\n', toc( start ) );
+    clear begin_time_disprog;
+    
+elseif floor( i * steps / n ) ~= floor( ( i - 1 ) * steps / n )
+    fprintf(' %g%%', floor( i * steps / n ) * ceil( 100 / steps ) );
+    
+end
 
 
