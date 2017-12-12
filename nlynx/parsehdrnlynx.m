8b4a389d801ed104cb3a0499035af409c20466bc
Francisco J. Flores <fjflores@neurostat.mit.edu>
2017-12-11 20:33:29 -0500

    Adds white space.

--- nlynx/parsehdrnlynx.m
+++ nlynx/parsehdrnlynx.m
@@ -34,37 +34,75 @@ if strcmp( class( hdr ), 'cell' )
     if old == true
         info.Fs = str2double(...
             cell2mat( regexp( hdr{ 13 }, ' .+', 'match' ) ) );
+        
         info.ADChan = str2double(...
             cell2mat( regexp( hdr{ 18 }, ' .+', 'match') ) );
-        idxDate = cell2mat( regexp( hdr{ 3 }, '(..)/(..)/(.{4})', 'tokenExtents' ) );
+        
+        idxDate = cell2mat(...
+            regexp( hdr{ 3 }, '(..)/(..)/(.{4})', 'tokenExtents' ) );
+        
         info.date = [ hdr{ 3 } ( idxDate( 3, 1 ) : idxDate( 3, 2 ) )...
-            '-' hdr{ 3 }( idxDate( 1, : ) ) '-' hdr{3}(idxDate(2,:))];
-        info.timeOpen = regexp(hdr{3},'( ..):(.+):(.+)','match');
-        info.convFactor = str2double(cell2mat(regexp(hdr{15},' .+','match')));
-        info.timeClose = regexp(hdr{4},'( ..):(.+):(.+)','match');
-        info.filterLow = str2double(cell2mat(regexp(hdr{21},' .+','match')));
-        info.filterHigh = str2double(cell2mat(regexp(hdr{25},' .+','match')));
-        info.inpRange = str2double(cell2mat(regexp(hdr{19},' .+','match')));
-        info.inpInverted = lower(cell2mat(regexp(hdr{20},'True|False','match'))); 
+            '-' hdr{ 3 }( idxDate( 1, : ) ) '-' hdr{ 3 }( idxDate( 2, : ) ) ];
+        
+        info.timeOpen = regexp( hdr{ 3 }, '( ..):(.+):(.+)', 'match' );
+        
+        info.convFactor = str2double(...
+            cell2mat( regexp( hdr{ 15 }, ' .+', 'match' ) ) );
+        
+        info.timeClose = regexp(...
+            hdr{ 4 }, '( ..):(.+):(.+)', 'match' );
+        
+        info.filterLow = str2double(...
+            cell2mat( regexp( hdr{ 21 }, ' .+', 'match' ) ) );
+        
+        info.filterHigh = str2double(...
+            cell2mat( regexp( hdr{ 25 }, ' .+', 'match' ) ) );
+        
+        info.inpRange = str2double(...
+            cell2mat( regexp( hdr{ 19 }, ' .+', 'match' ) ) );
+        
+        info.inpInverted = lower(...
+            cell2mat( regexp( hdr{ 20 }, 'True|False', 'match' ) ) ); 
         
     elseif new == true
-        info.Fs = str2double(cell2mat(regexp(hdr{14},' .+','match')));
-        info.ADChan = str2double(cell2mat(regexp(hdr{20},' .+','match')));
+        info.Fs = str2double(...
+            cell2mat( regexp( hdr{ 14 },' .+','match' ) ) );
+        
+        info.ADChan = str2double(...
+            cell2mat( regexp( hdr{ 20 }, ' .+','match' ) ) );
+        
 %         idxDate = cell2mat(regexp(hdr{3},'(..)/(..)/(.{4})','tokenExtents'));
 %         date = [hdr{3}(idxDate(3,1):idxDate(3,2)) '-' hdr{3}(idxDate(1,:)) '-' hdr{3}(idxDate(2,:))];
-        info.timeOpen = regexp(hdr{3},'( ..):(.+):(.+)','match');
-        info.timeClose = regexp(hdr{4},'( ..):(.+):(.+)','match');
-        info.convFactor = str2double(cell2mat(regexp(hdr{16},' .+','match')));
-        info.inpInverted = lower(cell2mat(regexp(hdr{22},'True|False','match')));
-        info.filterLow = str2double(cell2mat(regexp(hdr{25},' .+','match')));
-        info.filterHigh = str2double(cell2mat(regexp(hdr{29},' .+','match')));
-        info.inpRange = str2double(cell2mat(regexp(hdr{21},' .+','match')));
-        info.inpInverted = lower(cell2mat(regexp(hdr{20},'True|False','match')));
-        info.dspDelEnable = lower( cell2mat( regexp( hdr{ 32 }, 'Disabled|Enabled', 'match' ) ) );
+
+        info.timeOpen = regexp( hdr{ 3 }, '( ..):(.+):(.+)', 'match' );
+        
+        info.timeClose = regexp( hdr{ 4 }, '( ..):(.+):(.+)', 'match' );
+        
+        info.convFactor = str2double(...
+            cell2mat( regexp( hdr{ 16 }, ' .+', 'match' ) ) );
+        
+        info.inpInverted = lower(...
+            cell2mat( regexp( hdr{ 22 }, 'True|False', 'match' ) )) ;
+        
+        info.filterLow = str2double(...
+            cell2mat( regexp( hdr{ 25 }, ' .+', 'match' ) ) );
+        
+        info.filterHigh = str2double(...
+            cell2mat( regexp( hdr{ 29 }, ' .+', 'match' ) ) );
+        
+        info.inpRange = str2double(... 
+            cell2mat( regexp( hdr{ 21 }, ' .+', 'match' ) ) );
+        
+        info.inpInverted = lower(...
+            cell2mat( regexp( hdr{ 20 }, 'True|False', 'match' ) ) );
+        
+        info.dspDelEnable = lower(...
+            cell2mat( regexp( hdr{ 32 }, 'Disabled|Enabled', 'match' ) ) );
+        
         switch dspDelEnable
             case 'disabled'
-                info.dspDelay = str2double( cell2mat( regexp( hdr{ 33 },...
-                    ' .+', 'match' ) ) );
+                info.dspDelay = str2double(...
+                    cell2mat( regexp( hdr{ 33 }, ' .+', 'match' ) ) );
                 
             case 'enabled'
                 info.dspDelay = 0;
