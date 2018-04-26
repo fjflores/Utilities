70f24f48bb1264703fcb0ed986dfb4cc1ca968d6
Francisco J. Flores <fjflores@mit.edu>
2018-04-26 16:47:25 -0400

    Updates help.

--- plots/ffsavefig.m
+++ plots/ffsavefig.m
@@ -1,23 +1,25 @@
 function ffsavefig( hFig, folder, figName )
-% FFSAVEFIG saves the figure to a specified directory.
+% FFSAVEFIG saves a png the figure to a specified directory.
 %
 % Usage:
-% ffsavefig( hFig, folder )
+% ffsavefig( hFig, folder, figName )
 %
 % Input:
 % hFig: figure handle.
 % folder: directory where you want the figure to be saved. If it does not
 %         exists, then it creates it.
-% figName: Name of the figure.
+% figName: Name of the figure to save.
 %
 % Output:
 % Saves the figure to the specified folder.
 
 % Save figure.
 set( hFig, 'PaperPositionMode', 'auto' )
+
 % If folder does not exist, create it.
 fig2save = fullfile( folder, figName );
 folderFlag = exist( folder, 'dir' );
+
 if folderFlag == 7
     fprintf( 'Printing figure %s\n', figName );
     print( hFig, fig2save, '-dpng', '-r300' )
