545d3c294d350916ffe8e6a933bd09a0a5a9a871
Francisco J. Flores <fjflores@mit.edu>
2018-08-23 10:54:48 -0400

    svefig:Adds input to choose file extension.

--- plots/ffsavefig.m
+++ plots/ffsavefig.m
@@ -1,14 +1,16 @@
-function ffsavefig( hFig, folder, figName, ext )
+function ffsavefig( hFig, dir, figName, ext )
 % FFSAVEFIG saves a png the figure to a specified directory.
 %
 % Usage:
-% ffsavefig( hFig, folder, figName )
+% ffsavefig( hFig, folder, figName, ext )
 %
 % Input:
 % hFig: figure handle.
-% folder: directory where you want the figure to be saved. If it does not
-%         exists, then it creates it.
+% dir: directory where you want the figure to be saved. If it does not
+%      exists, then it creates it.
 % figName: Name of the figure to save.
+% ext: format in which to save the figure. Rightn now only supports png at
+%      300 dpi, and eps in ps2 color format.
 %
 % Output:
 % Saves the figure to the specified folder.
@@ -17,26 +19,33 @@ function ffsavefig( hFig, folder, figName, ext )
 set( hFig, 'PaperPositionMode', 'auto' )
 
 % If folder does not exist, create it.
-fig2save = fullfile( folder, figName, '.', ext );
+fig2save = strcat( fullfile( dir, figName ), [ '.' ext ] );
 folderFlag = exist( folder, 'dir' );
 
 if folderFlag ~= 7
-    fprintf( 'Figure directory did not exist. Creating...\n' )
-    fprintf( '%s\n', folder )
+    msg1 = sprintf( 'Requested directory did not exist. Creating...\n' );
+    msg2 = sprintf( '%s\n', dir );
+    disp( msg1 )
+    disp( pad( msg1, 'left' ) )
     mkdir( folder )
+    
+else
+    msg1 = sprintf( 'Requested directory already exists.\n' );
+    disp( msg1 )
 
 end
 
+msg1 = sprintf( 'Printing figure %s in %s format...\n', figName, ext );
+disp( pad( msg1, 'left' )
 switch ext
     case 'png'
-        fprintf( 'Printing figure %s\n', figName );
         print( hFig, fig2save, '-dpng', '-r300' )
         
     case 'eps'
-        fprintf( 'Printing figure %s\n', figName );
-        print( hFig, fig2save, '-dpng', '-r300' )
+        print( hFig, fig2save, '-depsc2' )
         
     otherwise
-        error( 'Something weird' )
+        error( 'Invalid figure extension.' )
         
 end
+sprintf( 'Done.' )
