5330034cc49a3c97851605031926e865ef2d779d
Francisco J. Flores <fjflores@mit.edu>
2018-10-01 14:34:58 -0400

    Eliminates verbosity.

--- procustesbed.m
+++ procustesbed.m
@@ -12,24 +12,17 @@ function cutSig = procustesbed( signal, nTotal )
 % cutSig: longer or shorter signal.
 
 if length( signal ) < nTotal
-%     disp(' in shorter case')
     deltaLength = nTotal - length( signal );
-%     disp( [ '  delta = ' num2str( deltaLength ) ] )
     seg = nan( deltaLength, 1 );
     signal = [ signal; seg ];
     cutSig = signal;
-%     disp( [ '  final length: ' num2str( length(signal) ) ] )
     
 elseif length( signal ) > nTotal
-%     disp(' in longer')
     deltaLength = length( signal ) - nTotal;
-%     disp( [ '  delta = ' num2str( deltaLength ) ] )
     signal( end - deltaLength + 1 : end ) = [ ];
     cutSig  = signal;
-%     disp( [ '  final length: ' num2str( length(signal) ) ] )
     
 else
-    disp( 'Signal length exactly matches desired length' )
     cutSig = signal;
     return
     
