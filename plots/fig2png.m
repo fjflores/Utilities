e71306d5a9d4ad315a73ac744e4866269514ff9d
Francisco J. Flores <fjflores@mit.edu>
2019-05-07 15:40:57 -0400

    Works on open figures too.

--- plots/fig2png.m
+++ plots/fig2png.m
@@ -1,8 +1,13 @@
-function fig2png
+function fig2png( varargin )
 %FIG2PNG converts matlab figures to png.
 %
-% No inputs or outputs. Just run in the folder with figures.
+% If no input, goes over the curent folder to find 
+% .fig files, and converts them to png's.
+%
+% With one input, it must be a figure handle, and
+%it will convert that figure to png.
 
+if isempty( varargin )
 files = dir( '*.fig' );
 nFiles = length( files );
 
@@ -13,4 +18,10 @@ for i = 1 :nFiles
     print( '-dpng', [ fig2save '.png' ] );
     close( h );
     
+end
+
+elseif strcmp( varargin( 1 ), 'gcf' )
+    set( gcf )
+    print( '-dpng', [ fig2save '.png' ] );
+     
 end
