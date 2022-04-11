91189870fc6a21f51b4c487670cd09f5473245d5
fjflores <fjflores@mit.edu>
2022-04-11 08:52:01 -0400

    update testhdr* to load test file only once.

--- nlynx/testhdr2.m
+++ nlynx/testhdr2.m
@@ -1,65 +1,47 @@
-%% Test that output is structure
+% Test parse of header 1
 load( 'TestData\testHdr2' )
 hdrInfo = parsehdrnlynx( hdr );
-assert( isstruct( hdrInfo ) )
+
+% Precondition: test that output is structure
+assert( isstruct( hdrInfo ), 'Output is not a structure!' )
 
 %% Test day
-load( 'TestData\testHdr2' )
-hdrInfo = parsehdrnlynx( hdr )
 day = '2/8/2022';
 assert( strcmp( hdrInfo.day, day ) )
 
 %% Test time file was open
-load( 'TestData\testHdr2' )
-hdrInfo = parsehdrnlynx( hdr );
 timeOpen = '11:46:40';
 assert( strcmp( hdrInfo.timeOpen, timeOpen ) )
 
 %% Test time file was closed
-load( 'TestData\testHdr2' )
-hdrInfo = parsehdrnlynx( hdr );
 timeClose = '13:54:48';
 assert( strcmp( hdrInfo.timeClose, timeClose ) )
 
 %% Test Fs
-load( 'TestData\testHdr2' )
-hdrInfo = parsehdrnlynx( hdr );
 Fs = 1600;
 assert( abs( hdrInfo.Fs - Fs ) < eps )
 
 %% Test conversion factor
-load( 'TestData\testHdr2' )
-hdrInfo = parsehdrnlynx( hdr );
 convFactor = 0.000000030518510385491027;
 assert( abs( hdrInfo.convFactor - convFactor ) < eps )
 
 %% Test AD name
-load( 'TestData\testHdr2' )
-hdrInfo = parsehdrnlynx( hdr );
 ADName = 'CSC1';
 assert( strcmp( hdrInfo.ADName, ADName ) );
 
 %% Test AD channel
-load( 'TestData\testHdr2' )
-hdrInfo = parsehdrnlynx( hdr );
 ADChan = 31;
 assert( abs( hdrInfo.ADChan - ADChan ) < eps )
 
 %% Test inverted input
 % Inverted input is false.
-load( 'TestData\testHdr2' )
-hdrInfo = parsehdrnlynx( hdr );
 assert( islogical( hdrInfo.inverted ) && ~hdrInfo.inverted )
 
 %% Test enabling of DSP delay
 % delayEnabled is false.
-load( 'TestData\testHdr2' )
-hdrInfo = parsehdrnlynx( hdr );
 assert( islogical( hdrInfo.delayEnabled ) && ~hdrInfo.delayEnabled )
 
 %% Test DSP delay value
-load( 'TestData\testHdr2' )
-hdrInfo = parsehdrnlynx( hdr );
 delay = 1969;
 assert( abs( hdrInfo.delay - delay ) < eps )
 
--- nlynx/testhdr3.m
+++ nlynx/testhdr3.m
@@ -1,64 +1,46 @@
-%% Test that output is structure
+% Test parse of header 1
 load( 'TestData\testHdr3' )
 hdrInfo = parsehdrnlynx( hdr );
-assert( isstruct( hdrInfo ) )
+
+% Precondition: test that output is structure
+assert( isstruct( hdrInfo ), 'Output is not a structure!' )
 
 %% Test day
-load( 'TestData\testHdr3' )
-hdrInfo = parsehdrnlynx( hdr );
 day = '2022/03/22';
 assert( strcmp( hdrInfo.day, day ) )
 
 %% Test time file was open
-load( 'TestData\testHdr3' )
-hdrInfo = parsehdrnlynx( hdr );
 timeOpen = '12:44:40';
 assert( strcmp( hdrInfo.timeOpen, timeOpen ) )
 
 %% Test time file was closed
-load( 'TestData\testHdr3' )
-hdrInfo = parsehdrnlynx( hdr );
 timeClose = '15:11:00';
 assert( strcmp( hdrInfo.timeClose, timeClose ) )
 
 %% Test Fs
-load( 'TestData\testHdr3' )
-hdrInfo = parsehdrnlynx( hdr );
 Fs = 1600;
 assert( abs( hdrInfo.Fs - Fs ) < eps )
 
 %% Test conversion factor
-load( 'TestData\testHdr3' )
-hdrInfo = parsehdrnlynx( hdr );
 convFactor = 0.000000061035156250000001;
 assert( abs( hdrInfo.convFactor - convFactor ) < eps )
 
 %% Test AD name
-load( 'TestData\testHdr3' )
-hdrInfo = parsehdrnlynx( hdr );
 ADName = 'CSC25';
 assert( strcmp( hdrInfo.ADName, ADName ) );
 
 %% Test AD channel
-load( 'TestData\testHdr3' )
-hdrInfo = parsehdrnlynx( hdr );
 ADChan = 23;
 assert( abs( hdrInfo.ADChan - ADChan ) < eps )
 
 %% Test inverted input
 % Inverted input ios false.
-load( 'TestData\testHdr3' )
-hdrInfo = parsehdrnlynx( hdr );
 assert( islogical( hdrInfo.inverted ) && ~hdrInfo.inverted )
 
 %% Test enabling of DSP delay 
-load( 'TestData\testHdr3' )
-hdrInfo = parsehdrnlynx( hdr );
 assert( islogical( hdrInfo.delayEnabled ) && hdrInfo.delayEnabled )
 
 %% Test DSP delay value
-load( 'TestData\testHdr3' )
-hdrInfo = parsehdrnlynx( hdr );
 delay = 1969;
 assert( abs( hdrInfo.delay - delay ) < eps )
 
