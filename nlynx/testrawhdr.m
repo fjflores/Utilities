8b21034fa0117bff7a73e71c919de287038e1085
fjflores <fjflores@mit.edu>
2022-04-11 11:06:16 -0400

    adds test for errors in getrawdhr.

--- nlynx/testrawhdr.m
+++ nlynx/testrawhdr.m
@@ -1,9 +1,47 @@
-%% Test that output is cell
-fileName = 'testData\CSC25.ncs';
-hdrRaw = getrawhdr( fileName );
-assert( iscell( hdrRaw ) )
+function tests = testrawhdr
+tests = functiontests( localfunctions );
 
-%% Test without extension
-fileName = 'testData\CSC25';
+end
+
+% Test that output is cell with normal call
+function testiscell( testCase )
+fileName = 'testData\sampleCSC3-25.ncs';
 hdrRaw = getrawhdr( fileName );
-assert( iscell( hdrRaw ) )
+verifyClass( testCase, hdrRaw, 'cell' )
+
+end
+
+% Test without extension but correct file type
+function testwoext( testCase )
+fileName = 'testData\sampleCSC3-25';
+hdrRaw = getrawhdr( fileName );
+verifyClass( testCase, hdrRaw, 'cell' )
+
+end
+
+% Test error with incorrect file type with extension
+function testwrongtype( testCase )
+fileName = 'testData\sampleSE.nse';
+actual = @() getrawhdr( fileName );
+errType = 'Utilities:Nlynx';
+verifyError( testCase, actual, errType )
+
+end
+
+% Test error with incorrect file type wo extension
+function testwrongtypenoext( testCase )
+fileName = 'testData\sampleSE';
+actual = @() getrawhdr( fileName );
+errType = 'Utilities:Nlynx';
+verifyError( testCase, actual, errType )
+
+end
+
+% Test error with wrong file.
+function testwrongfile( testCase )
+fileName = 'testData\testHdr1.mat';
+actual = @() getrawhdr( fileName );
+errType = 'Utilities:Nlynx';
+verifyError( testCase, actual, errType )
+
+end
