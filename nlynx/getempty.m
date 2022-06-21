0e0df01ba4c57f7767c194809f4d101c4af26967
Francisco J. Flores <fjflores@mit.edu>
2022-06-21 15:01:44 -0400

    add an option to read some files only.

--- nlynx/getempty.m
+++ nlynx/getempty.m
@@ -1,15 +1,37 @@
-function [ emptyFiles, fName ] = getempty( recDir )
+function [ emptyFiles, fName ] = getempty( recDir, fList )
+% GETEMPTY find empty csc/ncs files and returns boolean list.
+% 
+% Usage:
+% [ emptyFiles, fName ] = getempty( recDir )
+% [ emptyFiles, fName ] = getempty( recDir, fList )
+% 
+% Input:
+% recDir: path to directory with csc/ncs files.
+% fList: Optional. List of files to process as cell array.
+% 
+% Output:
+% emptyFiles: boolean list with true for each empty csc/ncs file.
+% fName: file names.
+
 
 ext = '*.ncs';
-files = dir( fullfile( recDir, ext ) );
-minBytes = 16384; % from actual empty files
-
-% throw an exception if the dir contains no ncs files.
-if isempty( files )
-    error( 'getempty:noncsfiles', 'No .ncs files in this directory' )
+if nargin == 1
+    files = dir( fullfile( recDir, ext ) );
+    % throw an exception if the dir contains no ncs files.
+    if isempty( files )
+        error( 'getempty:noncsfiles', 'No .ncs files in this directory' )
+        
+    end
+    
+else
+    for idx = 1 : length( fList )
+        files( idx ) = dir( fullfile( recDir, fList{ idx } ) );
+        
+    end
     
 end
 
+minBytes = 16384; % from actual empty files
 if ~isempty( files )
     nFiles = length( files );
     emptyFiles = false( nFiles, 1 );
@@ -25,7 +47,7 @@ if ~isempty( files )
             emptyFiles( fileIdx ) = true;
             
         end
-            
+        
     end
     
 end
--- nlynx/testempty.m
+++ nlynx/testempty.m
@@ -1,14 +1,12 @@
 % Load known connected files
-recDir = 'D:\Code\Matlab\Utilities\nlynx\TestData';
+recDir = 'E:\Code\Matlab\Utilities_akira\nlynx\TestData';
 
 %% Test connected status
 connTst = [ false false false false true ]';
-
 [ emptyFiles, fName ] = getempty( recDir );
 assert( isequal( emptyFiles, connTst ) )
 
 %% Test file names
-recDir = 'D:\Code\Matlab\Utilities\nlynx\TestData';
 fnTst =  {...
     'sampleCSC2-17.ncs';...
     'sampleCSC2-28.ncs';...
@@ -16,4 +14,21 @@ fnTst =  {...
     'sampleCSC3-25.ncs';...
     'sampleEmptyCSC3-7.ncs' };
 [ emptyFiles, fName ] = getempty( recDir );
+assert( isequal( fnTst, fName ) )
+
+%% Test some file connected
+connTst = [ false false true ]';
+fnTst =  {...
+    'sampleCSC2-17.ncs';...
+    'sampleCSC2-28.ncs';...
+    'sampleEmptyCSC3-7.ncs' };
+[ emptyFiles, fName ] = getempty( recDir, fnTst );
+assert( isequal( emptyFiles, connTst ) )
+
+%% Test some file names
+fnTst =  {...
+    'sampleCSC2-17.ncs';...
+    'sampleCSC2-28.ncs';...
+    'sampleEmptyCSC3-7.ncs' };
+[ emptyFiles, fName ] = getempty( recDir, fnTst );
 assert( isequal( fnTst, fName ) )
