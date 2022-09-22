f830f8e053f1916f0627e73f4ae8b2dfad1d7ef5
Francisco J. Flores <fjflores@mit.edu>
2022-09-21 23:16:19 -0400

    get power for several different bands.

--- spec/powerperband.m
+++ spec/powerperband.m
@@ -1,41 +1,44 @@
 function P = powerperband( S, f, fBand, modeP )
 %POWERPERBAND computes power for a given frequency band f1 to f2.
-% 
+%
 % Usage:
 % P = powerperband( spec, f, fBand, modeP )
-% 
+%
 % Input:
 % S: time-frequency spectrogram or coheregram.
 % f: all frequencies vector.
 % fBand: two-element vector with limits of frequency band.
-% modeP: how the marginal is computed: 
+% modeP: how the marginal is computed:
 %     'mean': mean power across the frequency band.
 %     'total': total power as area under the curve (integral).
 %     'median': median power across frequency band.
-% 
+%
 % Output:
 % P: power across the frequency band.
 
-f1 = fBand( 1 );
-f2 = fBand( 2 );
-idxBand = f >= f1 & f <= f2;
-SBand = S( :, idxBand );
-
-switch modeP
-    case 'total'
-        P = trapz( f( idxBand ), SBand, 2 );
-        
-    case 'mean'
-        P = mean( SBand, 2 );
-        
-    case 'median'
-        P = median( SBand, 2 );
+nBands = size( fBand, 1 );
+for bandIdx = 1 : nBands
+    thisBand = fBand( bandIdx, : );
+    f1 = thisBand( 1 );
+    f2 = thisBand( 2 );
+    fIdx = f >= f1 & f <= f2;
+    SBand = S( :, fIdx );
     
-    otherwise
-        error( 'Not valid mode for computing marginal' )
-       
+    switch modeP
+        case 'total'
+            P( :, bandIdx ) = trapz( f( fIdx ), SBand, 2 );
+            
+        case 'mean'
+            P( :, bandIdx ) = mean( SBand, 2 );
+            
+        case 'median'
+            P( :, bandIdx ) = median( SBand, 2 );
+            
+        otherwise
+            error( 'Not valid mode for computing marginal' )
+            
+    end
+    
 end
-
-
-
-
+    
+    
