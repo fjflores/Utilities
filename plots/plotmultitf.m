d1a539a0ee638f74bccf3bdc6333f309b93f575d
Francisco J. Flores <fjflores@mit.edu>
2023-08-11 10:54:27 -0400

    update plotmultitf to have name-value parmaters.

--- plots/plotmultitf.m
+++ plots/plotmultitf.m
@@ -1,20 +1,58 @@
-function hAx = plotmultitf( specMat, rows, cols, trans )
-% PLOTMULTISPEC makes a figure with multiple spectra/coherence.
+function hAx = plotmultitf( t, f, specMat, dims, varargin )
+% PLOTMULTITF makes a figure with multiple timefrequency charts.
 %
 % Usage:
-% plotmultispec( specMat, rows, cols, kind )
+% plotmultitf( t, f, specMat, dims )
+% plotmultitf( t, f, specMat, dims, Name, Value )
 %
+% The function assumes that all the time-frequency charts have the same
+% size.
+% 
 % Input:
-% specMat: tensor with freqs x times x trials/events.
-% rows: number of rows to plot.
-% cols: number of columsn to plot.
-% kind: defines the data transformation.
-% specIdx: indices of specs to plot. Length must be equal to rows x cols.
+% t: timestamps vector for x-axis.
+% f: frequency vector for y-axis.
+% specMat: tensor with freqs x times x charts.
+% dims: two-element vector with [ rows cols ] for subplot.
+% transform: transformation to apply. 'spec' for 10*log10, 'coher' 
+% for atanh. 'none' for none.
+% labels: cell array text to display in the top left corner of the chart, 
+% identifying it to the general public.
 %
 % Output:
-% figure with spectrograms or coheregrams.
+% figure with a montage of time-frequency charts.
 
-% Check input correcteness.
+% set up name-value pairs
+transform = 'none';
+
+nCharts = size( specMat, 3 );
+for i = 1 : nCharts
+    labels{ i } = i;
+
+end
+clear i
+
+% Parse  name-value pairs
+names = lower( varargin( 1 : 2 : end ) );
+values = varargin( 2 : 2 : end );
+for k = 1 : numel( names )
+    switch lower( names{ k } )
+        case "transform"
+            transform = values{ k };
+            
+        case "labels"
+            labels = values{ k };
+            
+        otherwise
+            error( '''%s'' is not a valid Name for Name, Value pairs.',...
+                names{ k } )
+            
+    end
+    
+end
+
+% Check input correctness.
+rows = dims( 1 );
+cols = dims( 2 );
 nPlots = rows * cols;
 nSpecs = size( specMat, 3 );
 if nSpecs > nPlots
@@ -26,7 +64,7 @@ if nSpecs > nPlots
 end
 
 % Define standardization fx.
-switch trans
+switch transform
     case 'spec'
         stdFx = @( x ) 10 * log10( x );
         barLab = 'power (db)';
@@ -36,7 +74,6 @@ switch trans
         barLab = 'z-coherence (au)';
         
     otherwise
-        warning( 'No transformation.' )
         stdFx = @( x ) 1 * x;
         barLab = 'N/A';
         
@@ -49,7 +86,7 @@ for plotIdx = 1 : nSpecs
     thisSpec =  squeeze( specMat( :, :, plotIdx ) );
     spec2plot = stdFx( thisSpec );
     allLims( plotIdx, 1 : 2 ) = prctile( spec2plot( : ), [ 5 99 ] );
-    imagesc( spec2plot' );
+    imagesc( t, f, spec2plot' );
     axis xy
     axis off
     colormap( magma )
@@ -60,12 +97,22 @@ for plotIdx = 1 : nSpecs
     posX = xlim( 1 ) + xlim( 2 ) * 0.025;
     posY = ylim( 2 ) - ylim( 2 ) * 0.05;
     
-    msg = sprintf( '%u', plotIdx );
+    msg = sprintf( '%s', num2str( labels{ plotIdx } ) );
     text( posX, posY, msg, 'Color', 'w', 'FontWeight', 'bold' )
     
+    if plotIdx == 1
+        axis on
+        set( hAx( plotIdx ), 'XTickLabel', {} )
+        box off
+
+    end
+
     if plotIdx == nSpecs
         ffcbar( gcf, gca, barLab );
-        
+        axis on
+        set( hAx( plotIdx ), 'YTickLabel', {} )
+        box off
+
     end
     
 end
