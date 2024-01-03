b617d7853b82ec3170107c7a9ec7552dcce3a87f
Isabella Dalla Betta <idallabe@mit.edu>
2024-01-03 18:01:27 -0500

    added axis option to choose which axis to jit

--- plots/scatterjit.m
+++ plots/scatterjit.m
@@ -1,11 +1,17 @@
 function s = scatterjit( varargin )
 % SCATTERJIT is a wrapper function for SCATTER where you can input the
-% amount of x-axis jitter desired. 
+% amount of jitter desired. 
 % 
 % Input arguments should be the same as SCATTER, with the addition of 
-% Name-Value pair:
+% Name-Value pairs:
 %   'Jit' or 'Jitter': the amount of jitter desired. Note: 0.1 jitter will
-%   lead to points jittered +/-0.1 around the original x value.
+%       lead to points jittered +/-0.1 around the original value.
+%   'Axis': which axis to jitter around. Options: 'x' (default), 'y', or 
+%       'both'. If both, 2 jitter values must be listed: [ x, y ]. If only
+%       one is listed, it will jitter the same amount in both axes.
+
+% Set default values.
+ax2jit = 'x';
 
 % Find the relevant Name-Value pair.
 args = varargin;
@@ -13,9 +19,13 @@ for idx = 1 : length( args )
     thisArg = args{ idx };
     if ~isnumeric( thisArg )
         switch lower( thisArg )
-            case { 'jit', 'jitter', 'xjit', 'xjitter' }
+            case { 'jit', 'jitter' }
                 jitIdx = idx;
-                xJit = args{ jitIdx + 1 };
+                jit = args{ jitIdx + 1 };
+                
+            case { 'axis' }
+                axIdx = idx;
+                ax2jit = args{ axIdx + 1 };
                 
         end
         
@@ -23,24 +33,73 @@ for idx = 1 : length( args )
     
 end
 
-if ~exist( 'jitIdx', 'var' ) % Deals with cases where function is called 
+if ~exist( 'jitIdx', 'var' ) % Deal with cases where function is called 
 % without a jitter N-V pair.
-    xJit = 0;
+    jit = 0;
     args4scat = args;
     warning( 'No jitter amount selected.' )
     
-else % Removes jitter N-V pair from argument list.
+else % Remove jitter N-V pair from argument list.
     idx4scat = [ 1 : jitIdx - 1, jitIdx + 2 : length( args ) ];
     args4scat = args( idx4scat );
     
 end
 
-% Create the random jitter vector and add it to the x values.
-jit = -xJit + ( 2 * xJit ) * rand( length( args{ 1 } ), 1 );
+% Remove axis N-V pair from argument list.
+if exist( 'axIdx', 'var' )
+    % Find new index of 'Axis' N-V pair.
+    for idx = 1 : length( args4scat )
+        thisArg = args4scat{ idx };
+        if ~isnumeric( thisArg )
+            switch lower( thisArg )
+                case { 'axis' }
+                    newAxIdx = idx;
+            end
+        end
+    end
+    
+    idx4scat = [ 1 : newAxIdx - 1, newAxIdx + 2 : length( args4scat ) ];
+    args4scat = args4scat( idx4scat );
+    
+end
+
+% Unpacks jit into jit values for each axis.
+switch ax2jit
+    case { 'x', 'X' }
+        xJit = jit;
+        yJit = 0;
+        
+    case { 'y', 'Y' }
+        xJit = 0;
+        yJit = jit;
+        
+    case { 'both', 'xy', 'XY' }
+        if numel( jit ) == 2
+            xJit = jit( 1 );
+            yJit = jit( 2 );
+            
+        elseif numel( jit ) == 1
+            xJit = jit;
+            yJit = jit;
+            
+        end
+        
+end
+
+% Create the random jitter vector(s) and add it to the values.
+xJitArray = -xJit + ( 2 * xJit ) * rand( length( args{ 1 } ), 1 );
+yJitArray = -yJit + ( 2 * yJit ) * rand( length( args{ 1 } ), 1 );
+
 if iscolumn( args{ 1 } )
-    args4scat{ 1 } = args{ 1 } + jit;    
+    args4scat{ 1 } = args{ 1 } + xJitArray;    
 elseif isrow( args{ 1 } )
-    args4scat{ 1 } = args{ 1 } + jit';    
+    args4scat{ 1 } = args{ 1 } + xJitArray';    
+end
+
+if iscolumn( args{ 2 } )
+    args4scat{ 2 } = args{ 2 } + yJitArray;    
+elseif isrow( args{ 1 } )
+    args4scat{ 2 } = args{ 2 } + yJitArray';    
 end
 
 % Pass all the arguments minus the jitter N-V pair to SCATTER function.
