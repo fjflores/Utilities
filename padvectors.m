c84e765e987bfbfe36dde00af11ec1752fe9ba6f
Francisco J. Flores <fjflores@mit.edu>
2024-08-27 14:12:26 -0400

    have a variable number of output arguments.

--- padvectors.m
+++ padvectors.m
@@ -1,4 +1,4 @@
-function paddedVecs = padvectors( varargin )
+function varargout = padvectors( varargin )
 
 % Get the number of input vectors
 num_vectors = nargin;
@@ -7,9 +7,9 @@ num_vectors = nargin;
 max_size = max( cellfun( @numel, varargin ) );
 
 % Pad the shorter vectors with zeros
-paddedVecs = cell( 1, num_vectors );
+% paddedVecs = cell( 1, num_vectors );
 for i = 1 : num_vectors
-    paddedVecs{ i } = [...
+    varargout{ i } = [...
         varargin{ i }, zeros( 1, max_size - numel( varargin{ i } ) ) ];
 
 end
