ca8af880df00ecf84d6b98693f6090a10ea38033
Francisco J. Flores <fjflores@mit.edu>
2024-08-27 15:02:18 -0400

    add method and help.

--- padvectors.m
+++ padvectors.m
@@ -1,7 +1,32 @@
 function varargout = padvectors( varargin )
+% PADVECTORS adds elements to match the longest vector.
+% 
+% Usage:
+% [ a1, b1, c1, ... ] = padvectors( a, b, c, ... )
+% [ a1, b1, c1, ... ] = padvectors( a, b, c, ..., method )
+% 
+% Input:
+% a, b, c: vectors to pad. The longest vector will be used as reference.
+% method: Optional. Method to pad the vectors. Can be "zeros" to pad with
+% zeros, "nans" to pad with nan's, or "linear" to linearly continue the
+% sequence. Note this last option only works well for monotonically
+% increasing vectors. Default: "zeros".
+% 
+% Output:
+% a1, b1, c1: padded vectors, except for the original ongest on e that
+% stays the same.
 
-% Get the number of input vectors
-num_vectors = nargin;
+% Deal with the last element being a string
+if isstring( varargin{ end } )
+    method = varargin{ end };
+    varargin{ end } = [];
+    num_vectors = nargin - 1;
+
+else
+    method = "zeros";
+    num_vectors = nargin;
+
+end
 
 % Find the maximum size among the vectors
 max_size = max( cellfun( @numel, varargin ) );
@@ -9,8 +34,27 @@ max_size = max( cellfun( @numel, varargin ) );
 % Pad the shorter vectors with zeros
 % paddedVecs = cell( 1, num_vectors );
 for i = 1 : num_vectors
-    varargout{ i } = [...
-        varargin{ i }, zeros( 1, max_size - numel( varargin{ i } ) ) ];
+
+    switch method
+
+        case "zeros"
+            varargout{ i } = [...
+                varargin{ i },...
+                zeros( 1, max_size - numel( varargin{ i } ) ) ];
+
+        case "nans"
+            varargout{ i } = [...
+                varargin{ i },...
+                nan( 1, max_size - numel( varargin{ i } ) ) ];
+
+        case "linear"
+            spacing = diff( varargin{ i } );
+            el2add = max_size - numel( varargin{ i } );
+            varargout{ i } = [...
+                varargin{ i },...
+                varargin{ i }( end ) + ( ( 1 : el2add ) .* spacing( 1 ) ) ];
+
+    end
 
 end
 
