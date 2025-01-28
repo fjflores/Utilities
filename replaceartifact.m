04a8d61655b983273aae93e31f4a6075b8eae887
Isabella Dalla Betta <idallabe@mit.edu>
2025-01-27 20:02:54 -0500

    fix replaceartifact so can handle multiple signals

--- replaceartifact.m
+++ replaceartifact.m
@@ -1,20 +1,25 @@
 function [ dataClean, remIdx, tClean ] = replaceartifact( signal, t, artTimes, meth )
-%REPLACEARTIFACT replaces artifacts with nans.
+%REPLACEARTIFACT replaces or removes artifacts, depending on desired method.
 %
 % Usage:
 % dataClean = replaceartifact( signal, t, artTimes, meth )
 % [ dataClean, remIdx ] = replaceartifact( signal, t, artTimes, meth )
+% [ dataClean, remIdx, tClean ] = replaceartifact( signal, t, artTimes, meth )
 %
 % Input:
-% signal: column vector to be processed.
+% signal: column vector to be processed. Can be multiple signals (each in a
+% column) as long as the timestamp vector applies to all columns.
 % t: timestamps for the signal vector.
-% artTimes = two-colum matrix of start and end times for artifacts.
-% meth: 'nan' to replace withs nan's, 'linear' to interpolate
-% with a linear function, 'none' to just delete the data. This also
-% outputs the removed time.
+% artTimes: two-colum matrix of start and end times for artifacts.
+% meth: 'nan' to replace withs nan's, 'zeros' to replace with zeros,
+% 'linear' to interpolate with a linear function, 'none' to just delete the
+% data. This also outputs the removed time.
 %
 % Output:
-% dataClean = data artifacts removed.
+% dataClean: data with artifacts removed.
+% remIdx: indeces of removed signal.
+% tClean: updated timestamp vector without removed signals (only for method
+% 'none').
 %
 % Beware that looks for times, not points (unsafe, but ok as long as only
 % one signal is being processed).
@@ -54,11 +59,11 @@ switch meth
         fprintf( 'Replacing with nans\n' )
         % replaces artifacts with nans
         dataClean = signal;
-        dataClean( remIdx ) = nan;
+        dataClean( remIdx, : ) = nan;
 
     case( 'zeros' )
         dataClean = signal;
-        dataClean( remIdx ) = 0;
+        dataClean( remIdx, : ) = 0;
 
     case( 'none' )
         fprintf( 'Removing bad data\n' )
@@ -70,22 +75,30 @@ switch meth
         
     case( 'linear' )
         fprintf( 'Replacing with nearest linear fit\n' )
-        % replaces artifacts with linear interpolation
-        % find discontinuity points
-        dataClean = signal;
-        disIdx = diff( remIdx);
+        disIdx = diff( remIdx );
         upIdx = find( disIdx == 1 );
         dnIdx = find( disIdx == -1 ) + 1;
-        
-        % piecewise replacement
-        for repIdx = 1 : length( upIdx )
-            p = polyfit(...
-                [ t( upIdx( repIdx ) ) t( dnIdx( repIdx ) ) ],...
-                [ signal( upIdx( repIdx ) ) signal( dnIdx( repIdx ) ) ],...
-                1 );
-            fitLine = polyval( p, t( upIdx( repIdx ) : dnIdx( repIdx ) ) );
-            dataClean( upIdx( repIdx ) : dnIdx( repIdx ) ) = fitLine;
-            
+
+        assert( ~isempty( upIdx ) & ~isempty( dnIdx ),...
+            [ 'Cannot perform linear interpolation bc artifact ',...
+            'includes beginning or end of signal.' ] )
+
+        % replaces artifacts with linear interpolation
+        % find discontinuity points
+        for sigIdx = 1 : size( signal, 2 )
+            dataClean( :, sigIdx ) = signal( :, sigIdx );
+
+            % piecewise replacement
+            for repIdx = 1 : length( upIdx )
+                p = polyfit(...
+                    [ t( upIdx( repIdx ) ) t( dnIdx( repIdx ) ) ],...
+                    [ signal( upIdx( repIdx ), sigIdx ) signal( dnIdx( repIdx ), sigIdx ) ],...
+                    1 );
+                fitLine = polyval( p, t( upIdx( repIdx ) : dnIdx( repIdx ) ) );
+                dataClean( upIdx( repIdx ) : dnIdx( repIdx ), sigIdx ) = fitLine;
+
+            end
+
         end
         
     otherwise
